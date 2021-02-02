81460d4b636e8fa2a7a138985f28eb8a01ea7fe7
_lhj__ <960991815@qq.com>
2021-02-02 16:24:45 +0800

    李四后提交

--- 01-git基础认识/src/com/lin/demotest/UserServiceJava.java
+++ 01-git基础认识/src/com/lin/demotest/UserServiceJava.java
@@ -4,12 +4,12 @@ public interface UserServiceJava {
 	//添加了一个adduser方法
 	void addUser();
 	
-	
-
 	//添加了一个adduser2方法
-		void addUser2();
+	void addUser2();
 		
-		//添加了一个adduser3方法
-				void addUser3();
+	//添加了一个adduser3方法
+	void addUser3();
+	
+	void lisi();
 
 }
