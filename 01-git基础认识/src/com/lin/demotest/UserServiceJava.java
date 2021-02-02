e62f38acd55e9bfd3e3e93da836130a4e12e1218
_lhj__ <960991815@qq.com>
2021-02-02 14:42:56 +0800

    删除废物代码

--- 01-git基础认识/src/com/lin/demotest/UserServiceJava.java
+++ 01-git基础认识/src/com/lin/demotest/UserServiceJava.java
@@ -3,15 +3,5 @@ package com.lin.demotest;
 public interface UserServiceJava {
 	//添加了一个adduser方法
 	void addUser();
-	//又添加了一个删除方法
-	void delete();
 	
-	//添加xxx
-	void xxx();
-	
-	//添加一个aaa
-	void aaa();
-	
-	//添加一个bbb
-		void bbb();
 }
