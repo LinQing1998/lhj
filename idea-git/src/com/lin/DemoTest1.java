27263b39862252c733a7aecfe2ed282114723384
_lhj__ <960991815@qq.com>
2021-02-02 22:41:37 +0800

    idea push新项目

---
+++ idea-git/src/com/lin/DemoTest1.java
@@ -0,0 +1,17 @@
+package com.lin;
+
+import javax.sound.midi.Soundbank;
+
+public class DemoTest1 {
+    void aa(){
+        System.out.println("aa");
+    }
+
+    void aa2(){
+        System.out.println("aa2");
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
